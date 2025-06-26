d2c442343bf9f009df259bc8b2082b65bf2a61f5
Konstantin <konstamah@gmail.com>
2025-06-26 11:41:24 +0300

    feat: narrowing task completed

---
+++ JS Practice TypeScript/project/dist/narrowing.js
@@ -0,0 +1,8 @@
+"use strict";
+function printElement(element) {
+    if (typeof element === 'number')
+        console.log(element);
+    else
+        console.log(element.length);
+}
+printElement("svshd");
---
+++ JS Practice TypeScript/project/src/narrowing.ts
@@ -0,0 +1,8 @@
+function printElement(element: number | string): void {
+    if (typeof element === 'number')
+        console.log(element);
+    else
+        console.log(element.length);
+}
+
+printElement(43483);
